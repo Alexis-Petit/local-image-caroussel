52a6adf896a07167c88bbd89684a2fb6c7375d77
thibaut moal <moal.thibaut@gmail.com>
2021-01-25 13:33:24 +0100

    passé de 4.40 à 5 min troudcul

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ function App() {
           <input defaultChecked={countdownValue===30000}type="radio" value="30000" name="gender" /> 30s
           <input defaultChecked={countdownValue===60000}type="radio" value="60000" name="gender" /> 1m
           <input defaultChecked={countdownValue===120000} type="radio" value="120000" name="gender" /> 2m
-          <input defaultChecked={countdownValue===280000} type="radio" value="280000" name="gender" /> 5m
+          <input defaultChecked={countdownValue===300000} type="radio" value="300000" name="gender" /> 5m
         </div>
     
       </header>
